997a0b14a7d4417c6dec5487d9445ed5864db600
minured <minured@qq.com>
2022-08-28 15:20:08 +0800

    feat: auth

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const registerRouter = require("./router");
 const app = express();
 
 app.use(express.json());
-app.use(core());
+// app.use(core());
 registerRouter(app);
 
 app.get("/api", (req, res) => res.send("jobs-server"));
 
 app.listen(port, () => {
-  console.log(`jobs-server is running on: localhost:${port}`);
+  console.log(`jobs-server is running on: http://localhost:${port}`);
 });
 
 module.exports = { app };
